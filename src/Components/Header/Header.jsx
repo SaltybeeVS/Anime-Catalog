be5c88f8ab784822693c9c5779e8f62c15c1b827
Vicente Sandoval <vgscarrasco@gmail.com>
2025-03-11 21:01:18 -0300

    Added target

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,9 +9,9 @@ function Header (){
             <h1><a href="#">VS AnimeList</a></h1>
             <nav>
                 <ul>
-                    <li><a href="https://github.com/SaltybeeVS"><FaGithub className='headerIcon'/></a></li>
-                    <li><a href='https://www.linkedin.com/in/vicente-sandoval-carrasco/'><FaLinkedin className='headerIcon'/></a></li>
-                    <li><a href='https://saltybeevs.github.io/Portfolio/'>See my portfolio</a></li>
+                    <li><a href="https://github.com/SaltybeeVS" target='_blank'><FaGithub className='headerIcon'/></a></li>
+                    <li><a href='https://www.linkedin.com/in/vicente-sandoval-carrasco/' target='_blank'><FaLinkedin className='headerIcon'/></a></li>
+                    <li><a href='https://saltybeevs.github.io/Portfolio/' target='_blank'>See my portfolio</a></li>
                 </ul>
             </nav>
         </header>
